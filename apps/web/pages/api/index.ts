e3fadccc5bab4708d196de98bdf568adea69485a
ihyafauzi.my.id <git@ihyafauzi.my.id>
2022-09-08 04:50:55 +0700

    Yes
    Signed-off-by: ihyafauzi.my.id <git@ihyafauzi.my.id>

--- apps/web/pages/api/index.ts
+++ apps/web/pages/api/index.ts
@@ -1,5 +1,5 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 
 export default async (_: NextApiRequest, res: NextApiResponse) => {
-    return res.redirect('https://github.com/hansputera/tiktok-dl.git');
+    return res.redirect('https://github.com/mioscape');
 };
